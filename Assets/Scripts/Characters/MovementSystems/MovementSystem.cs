f201c273e5e555fde6ceed3bbfe92476538c589d
Kla <naphat.watt@bumail.net>
2025-06-02 16:21:37 +0700

    Snap grid

--- Assets/Scripts/Characters/MovementSystems/MovementSystem.cs
+++ Assets/Scripts/Characters/MovementSystems/MovementSystem.cs
@@ -1,5 +1,4 @@
 using System;
-using DG.Tweening;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -19,12 +18,14 @@ namespace Characters.MovementSystems
         [SerializeField] private int moveVerticalDistance;
         
         [Unit(Units.Second)]
-        [SerializeField] private float moveDuration;
+        [SerializeField] private float moveHorizontalDuration;
+        
+        [Unit(Units.Second)]
+        [SerializeField] private float movVerticallDuration;
 
         [SerializeField] private bool snapGridOnStart;
 
         private bool _isGrounded;
-        private bool _canMove;
         private BoxCollider2D _boxCollider2D;
         
         private void Start()
@@ -38,24 +39,49 @@ namespace Characters.MovementSystems
             
             transform.position = SnapToGrid(transform.position);
         }
-
+        
         [Button]
         public void TryMove()
         {
             //if (!_isGrounded) return;
+
+            Debug.Log("MOVE");
+        }
+
+        private bool CheckCanMove()
+        {
             Bounds bounds = _boxCollider2D.bounds;
             Vector2 topCenter = bounds.center + new Vector3(0f, bounds.extents.y);
             Vector2 rayStartPos = topCenter + new Vector2(0, moveVerticalDistance);
-            if(Physics2D.Raycast(rayStartPos, Vector2.right, moveHorizontalDistance, collisionLayer)) return;
-            _canMove = true;
-            Debug.Log("MOVE");
+            Debug.DrawRay(rayStartPos, Vector2.right * moveHorizontalDistance, Color.red, 0.1f);
+            if (Physics2D.Raycast(rayStartPos, Vector2.right, moveHorizontalDistance, collisionLayer)) return false;
+            return true;
+        }
+
+        private bool CheckGround(Vector2 origin, out RaycastHit2D hit, float length)
+        {
+            hit = Physics2D.Raycast(origin, Vector2.down, length, collisionLayer);
+            Debug.DrawRay(origin, Vector2.down * length, hit.collider ? Color.green : Color.red, 0.1f);
+            return hit.collider != null;
         }
         
         private Vector2 SnapToGrid(Vector2 pos)
         {
-            float x = MathF.Floor(MathF.Abs(pos.x)) + gridOffset.x;
-            float y = MathF.Floor(MathF.Abs(pos.y)) + gridOffset.y;
-            return new Vector2(MathF.Sign(pos.x) * x, MathF.Sign(pos.y) * y);
+            float newX = MathF.Floor(pos.x) + gridOffset.x;
+            float newY = MathF.Floor(pos.y) + gridOffset.y;
+
+            Vector2 snappedPos = new Vector2(newX, newY);
+            float deltaY = pos.y - newY;
+
+            if (deltaY > 0 && CheckGround(pos, out RaycastHit2D hit, deltaY + 0.01f))
+            {
+                Debug.Log(hit.transform.name);
+                snappedPos = hit.point + hit.normal * 0.01f;
+            }
+               
+            
+           
+            return snappedPos;
         }
 
         private void OnTriggerEnter2D(Collider2D other)
