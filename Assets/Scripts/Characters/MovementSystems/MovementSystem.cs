f34dfec296e1b377915500d5df9727cb8fcc7dc8
Kla <naphat.watt@bumail.net>
2025-06-03 17:56:17 +0700

    Clean code

--- Assets/Scripts/Characters/MovementSystems/MovementSystem.cs
+++ Assets/Scripts/Characters/MovementSystems/MovementSystem.cs
@@ -8,27 +8,62 @@ namespace Characters.MovementSystems
 {
     public class MovementSystem : MonoBehaviour
     {
-        [Title("Configs")] [SerializeField] private Vector2 gridOffset = new Vector2(0.5f, 0);
+        #region Inspectors & Variables
+        
+        [Title("Configs")] [PropertyTooltip("Offset in the grid. Depends on the pivot point of transform.")]
+        [SerializeField] private Vector2 gridOffset = new Vector2(0.5f, 0);
+        
+        [PropertyTooltip("Layer to detected and perform collision with.")]
         [SerializeField] private LayerMask collisionLayer;
+        
+        [PropertyTooltip("Gravity scale of this entity. the sign is mean the gravity's direction.")]
         [SerializeField] private float gravityScale = -9.81f;
 
-        [Title("Movement Stats")] [PropertyTooltip("(Units)")] [SerializeField]
-        private int moveHorizontalDistance = 1;
+        [Title("Movement Stats")] [PropertyTooltip("Movement distance in horizontal axis (Units)")] 
+        [ValidateInput("@moveHorizontalDistance >= 0", "this value can't below than zero.")]
+        [SerializeField] private int moveHorizontalDistance = 1;
 
-        [PropertyTooltip("(Units)")] [SerializeField]
-        private int moveVerticalDistance = 1;
+        [PropertyTooltip("Movement distance in vertical axis (Units)")] 
+        [ValidateInput("@moveHorizontalDistance >= 0", "this value can't below than zero.")]
+        [SerializeField] private int moveVerticalDistance = 1;
 
-        [Unit(Units.Second)] [SerializeField] private float moveDuration = 0.25f;
+        [PropertyTooltip("The duration of movement action")]  [Unit(Units.Second)] 
+        [ValidateInput("@moveHorizontalDistance >= 0", "this value can't below than zero.")]
+        [SerializeField] private float moveDuration = 0.25f;
 
+        [InfoBox("You can adjust movement curve. The start and end point need to be zero. and the highest value should be one.")] 
         [SerializeField] private AnimationCurve moveCurve;
-        
-        [SerializeField] private bool snapGridOnStart;
 
+        [PropertyTooltip("Snap the position of this character with grid position, Suggest to be true.")] 
+        [SerializeField] private bool snapGridOnStart = true;
+
+        /// <summary>
+        /// Determine this character is on ground or not.
+        /// </summary>
         private bool _isGrounded;
+        
+        /// <summary>
+        /// Determine this character touch the ground from falling or not. Should be true when touch the ground first time from falling state.
+        /// </summary>
         private bool _isLanding;
+        
+        /// <summary>
+        /// True is mean use gravity calculation.
+        /// </summary>
         private bool _ignoreGravity;
+        
+        /// <summary>
+        /// The box collider of this character.
+        /// </summary>
         private BoxCollider2D _boxCollider2D;
         
+        #endregion
+
+        #region Unity Methods
+
+        /// <summary>
+        /// Initialize the character
+        /// </summary>
         private async void Start()
         {
             if (!snapGridOnStart) return;
@@ -37,16 +72,27 @@ namespace Characters.MovementSystems
                 Debug.LogWarning("Need BoxCollider2D with is Trigger");
                 return;
             }
-
+      
             await UniTask.WaitForSeconds(0.1f);
             transform.position = SnapToGrid(transform.position);
         }
 
+        /// <summary>
+        /// Calculate and control the gravity.
+        /// </summary>
         private void Update()
         {
             GravityHandler();
         }
 
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The primary movement of this game. Try to move and jump to the next grid, depend on the movement distance
+        /// Move when the condition is true and return ( not do any action ) if the condition is false.
+        /// </summary>
         [Button]
         public async void TryMoveAction()
         {
@@ -76,6 +122,11 @@ namespace Characters.MovementSystems
                 });
         }
 
+        /// <summary>
+        /// Move position method, every movement need to be call in this method,
+        /// Try to snap to the grid and ground in every movement changed.
+        /// </summary>
+        /// <param name="newPos"></param>
         private void MovePosition(Vector2 newPos)
         {
             if (newPos.y < transform.position.y)
@@ -95,6 +146,9 @@ namespace Characters.MovementSystems
             transform.position = newPos;
         }
 
+        /// <summary>
+        /// Handle the gravity of this character.
+        /// </summary>
         private void GravityHandler()
         {
             if (_ignoreGravity) return;
@@ -103,6 +157,11 @@ namespace Characters.MovementSystems
             MovePosition(newPos);
         }
 
+        /// <summary>
+        /// Check the obstacle in the front and top direction.
+        /// Detected ranges are depend on the movement direction, both of horizontal and vertical axis.
+        /// </summary>
+        /// <returns>True if they have the obstacle in front of this character movement and above.</returns>
         private bool CheckObstacle()
         {
             Bounds bounds = _boxCollider2D.bounds;
@@ -111,7 +170,14 @@ namespace Characters.MovementSystems
             Debug.DrawRay(rayStartPos, Vector2.right * moveHorizontalDistance, Color.red, 0.1f);
             return Physics2D.Raycast(rayStartPos, Vector2.right, moveHorizontalDistance, collisionLayer);
         }
-
+        
+        /// <summary>
+        /// Check the ground below.
+        /// </summary>
+        /// <param name="origin">Start position of ray.</param>
+        /// <param name="hit">Object or ground hit.</param>
+        /// <param name="length">Ray's length</param>
+        /// <returns>True if they have the ground and collision object below this character.</returns>
         private bool CheckGround(Vector2 origin, out RaycastHit2D hit, float length)
         {
             hit = Physics2D.Raycast(origin, Vector2.down, length, collisionLayer);
@@ -120,23 +186,25 @@ namespace Characters.MovementSystems
             return _isGrounded;
         }
 
+        /// <summary>
+        /// Snap the position in to grid system,
+        /// in vertical axis or x-axis will automatically snap.
+        /// in horizontal axis or y-axis will snap depends on the ground surface.
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <returns></returns>
         private Vector2 SnapToGrid(Vector2 pos)
         {
             float newX = MathF.Floor(pos.x) + gridOffset.x;
-            float newY = MathF.Round(pos.y) + gridOffset.y;
+            Vector2 snappedPos = new Vector2(newX, pos.y);
+            float deltaY = transform.position.y - pos.y;
 
-            Vector2 snappedPos = new Vector2(newX, newY);
-            float deltaY = pos.y - newY;
-
-            if (deltaY > 0 && CheckGround(pos, out RaycastHit2D hit, deltaY))
+            if (deltaY > 0 && CheckGround(pos, out RaycastHit2D hit, deltaY + 0.05f))
                 snappedPos = hit.point;
             
             return snappedPos;
         }
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            Debug.Log(other.transform.name);
-        }
+        
+        #endregion
     }
 }
