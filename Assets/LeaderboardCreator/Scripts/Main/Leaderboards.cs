1ce34ff6b4320742fc45cd3e03b7a9926b569726
Kla <naphat.watt@bumail.net>
2025-06-10 17:01:26 +0700

    Dan Leaderboard

---
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,7 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference ProjectHopper = new LeaderboardReference("7172738462773dd81d1365c9b9880d6aab84e630bc1ebfcfebb4667cd4b7ba4d");
+    }
+}
